224244167cf3c266a9b00106845e5ad089e64783
phamdat <phamdat@phamdats-MacBook-Pro.local>
2023-10-17 14:05:41 +0700

    set branch to return signal

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from datetime import datetime, timedelta, timezone
 import json
 # from flask import Flask, jsonify, request
 
-from model import convert, predict
+# from model import convert, predict
 import pandas as pd
 import numpy as np
 
@@ -42,27 +42,27 @@ async def pong():
     return {"ping": "pong!"}
 
 
-@app.post("/predict", status_code=200)
-def get_prediction():
-    # ticker = payload.ticker
+# @app.post("/predict", status_code=200)
+# def get_prediction():
+#     # ticker = payload.ticker
 
-    prediction_list = predict("ticker")
-    resp = []
-    prediction_list = prediction_list.tolist()
+#     prediction_list = predict("ticker")
+#     resp = []
+#     prediction_list = prediction_list.tolist()
 
-    result = datetime.today()
-    tz = timezone(timedelta(hours=7))
-    for i in range(len(prediction_list)):
-        result += timedelta(minutes=1)
-        response_object = {"timestamp": result.astimezone(tz), "price": prediction_list[i][0]}
-        resp.append(response_object)
+#     result = datetime.today()
+#     tz = timezone(timedelta(hours=7))
+#     for i in range(len(prediction_list)):
+#         result += timedelta(minutes=1)
+#         response_object = {"timestamp": result.astimezone(tz), "price": prediction_list[i][0]}
+#         resp.append(response_object)
     
-    df = pd.DataFrame(resp)
-    df.to_csv("signal.csv", sep=',', index=False, encoding='utf-8')
+#     df = pd.DataFrame(resp)
+#     df.to_csv("signal.csv", sep=',', index=False, encoding='utf-8')
 
-    # if not prediction_list:
-    #     raise HTTPException(status_code=400, detail="Model not found.")
+#     # if not prediction_list:
+#     #     raise HTTPException(status_code=400, detail="Model not found.")
 
-    # response_object = {"ticker": ticker, "forecast": convert(prediction_list)}
-    # return 
-    return [{"timestamp": result.astimezone(tz), "price":1.234}]
+#     # response_object = {"ticker": ticker, "forecast": convert(prediction_list)}
+#     # return resp
+#     return [{"timestamp": result.astimezone(tz), "price":1.234}]
